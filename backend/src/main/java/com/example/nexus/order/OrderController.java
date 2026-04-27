38958cce681d8bbe34284badc8c186a0ffa033b8
jdh <wjdehd555@gmail.com>
2026-04-27 11:07:03 +0900

    [Feat] #302 - OrderRepository 생성

--- backend/src/main/java/com/example/nexus/order/OrderController.java
+++ backend/src/main/java/com/example/nexus/order/OrderController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class OrderController {
-    private final OrderSerivce orderSerivce;
+    private final OrderService orderService;
 
 }

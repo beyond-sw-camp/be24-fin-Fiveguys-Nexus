726914ca2e4848751a8e336f070566d13ccf18f9
dwg245 <dwg0245@gmail.com>
2026-04-27 14:18:51 +0900

    [Feat] #331 - MENU Entity 생성

--- backend/src/main/java/com/example/nexus/menu/model/menu.java
+++ backend/src/main/java/com/example/nexus/menu/model/menu.java
@@ -1,4 +1,31 @@
 package com.example.nexus.menu.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "menu")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="menu_idx", nullable = false, unique = true)
+    private Long idx; // 메뉴 번호
+
+    @Column(name = "menu_name", nullable = false, unique = true)
+    private String menuName;
+
+    @Column(name = "price", nullable = false)
+    private String price;
+
+    @Column(name = "img_path", nullable = false)
+    private String imgPath;
+
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted = false;
+
 }

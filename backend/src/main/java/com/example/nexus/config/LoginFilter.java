845d0d78c039e36dd1f7c705675aa314d29b684d
KwonLaptop <chris03232000@gmail.com>
2026-04-27 19:57:57 +0900

    [Feat] #4 - 기본적인 로그인, 회원가입 기능 구현

--- backend/src/main/java/com/example/nexus/config/LoginFilter.java
+++ backend/src/main/java/com/example/nexus/config/LoginFilter.java
@@ -1,18 +1,41 @@
 package com.example.nexus.config;
 
+import com.example.nexus.user.model.UserDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
+    private final AuthenticationManager authenticationManager;
+
+    public LoginFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+        this.authenticationManager = authenticationManager;
+    }
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+
+        try {
+            UserDto.LoginReq dto = new ObjectMapper().readValue(request.getInputStream(), UserDto.LoginReq.class);
+
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword(), null);
+
+            return authenticationManager.authenticate(token);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
--- backend/src/main/java/com/example/nexus/config/SecurityConfig.java
+++ backend/src/main/java/com/example/nexus/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.example.nexus.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -12,6 +15,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final LoginFilter loginFilter;
+    private final AuthenticationConfiguration configuration;
+    private final JwtFilter jwtFilter;
+
+    @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
                 (auth) -> auth
@@ -19,7 +26,14 @@ public class SecurityConfig {
         );
 
         http.addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
-    }
 
+
+    http.csrf(AbstractHttpConfigurer::disable);
+    http.httpBasic(AbstractHttpConfigurer::disable);
+    http.formLogin(AbstractHttpConfigurer::disable);
+
+    http.addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class);
+    http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    return http.build();
+    }
 }

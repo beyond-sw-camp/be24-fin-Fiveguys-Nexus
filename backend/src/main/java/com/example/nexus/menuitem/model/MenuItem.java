fe6f7bbe3e5861beb9c501142d7d0716efcbcc67
dwg245 <dwg0245@gmail.com>
2026-04-27 14:43:29 +0900

    [Feat] #340 - Menu_item Entity 생성

---
+++ backend/src/main/java/com/example/nexus/menuitem/model/MenuItem.java
@@ -0,0 +1,25 @@
+package com.example.nexus.menuitem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "menu_item")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MenuItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "menu_item_idx", nullable = false, unique = true)
+    private Long idx;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private String menu_unit;
+
+}
--- backend/src/main/java/com/example/nexus/menuitem/model/menuItem.java
+++
@@ -1,4 +0,0 @@
-package com.example.nexus.menuitem.model;
-
-public class menuItem {
-}

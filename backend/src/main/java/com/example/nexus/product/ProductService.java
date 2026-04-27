2242617d515fe613615687d95e84a8c24b5272ad
shtmdcks06 <shtmdcks06@gmail.com>
2026-04-27 15:05:23 +0900

    [Feat] #339 - Product Service 생성

---
+++ backend/src/main/java/com/example/nexus/product/ProductService.java
@@ -0,0 +1,10 @@
+package com.example.nexus.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+}

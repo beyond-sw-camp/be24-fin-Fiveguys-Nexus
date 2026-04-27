9c2a66cd11ecd5bbd35e02e315220d901f4b84c8
hijaehyuk <messi300060@gmail.com>
2026-04-27 11:49:20 +0900

    [Feat] #312 - HeadInventory 도메인 기본 구조 생성

---
+++ backend/src/main/java/com/example/nexus/headinventory/HeadInventoryController.java
@@ -0,0 +1,11 @@
+package com.example.nexus.headinventory;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/headinventory")
+@RestController
+@RequiredArgsConstructor
+public class HeadInventoryController {
+}
--- backend/src/main/java/com/example/nexus/headinventory/model/headInventory.java
+++
@@ -1,4 +0,0 @@
-package com.example.nexus.headinventory.model;
-
-public class headInventory {
-}

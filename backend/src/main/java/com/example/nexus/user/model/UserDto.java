2e45ccbadd702f810fd8650113bc81e940d8a5fc
KwonLaptop <chris03232000@gmail.com>
2026-04-27 17:44:23 +0900

    [Feat] #5 - 로그인 요청, 응답 Dto 생성

---
+++ backend/src/main/java/com/example/nexus/user/model/UserDto.java
@@ -0,0 +1,29 @@
+package com.example.nexus.user.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+public class UserDto {
+
+    @Getter
+    public static class LoginReq {
+        private String email;
+        private String password;
+    }
+
+    @Builder
+    @Getter
+    public static class LoginRes {
+        private Long idx;
+        private String email;
+        private String name;
+
+        public static LoginRes from(User entity) {
+            return LoginRes.builder()
+                    .idx(entity.getIdx())
+                    .email(entity.getEmail())
+                    .name(entity.getUserName())
+                    .build();
+        }
+    }
+}

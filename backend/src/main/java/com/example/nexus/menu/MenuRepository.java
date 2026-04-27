ab55bb2138ac84ce2b5e8e239b92b8f305c66f3f
dwg245 <dwg0245@gmail.com>
2026-04-27 14:33:31 +0900

    [Fix] #331 - MenuRepository 띄어쓰기 추가

--- backend/src/main/java/com/example/nexus/menu/MenuRepository.java
+++ backend/src/main/java/com/example/nexus/menu/MenuRepository.java
@@ -3,5 +3,5 @@ package com.example.nexus.menu;
 import com.example.nexus.menu.model.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MenuRepository extends JpaRepository<Menu,Long> {
+public interface MenuRepository extends JpaRepository<Menu, Long> {
 }

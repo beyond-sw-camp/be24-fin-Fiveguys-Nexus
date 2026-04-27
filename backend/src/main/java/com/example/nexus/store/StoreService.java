3e49b7f1a27c344a98f800a590e5504e4cbeaf7f
dwg245 <dwg0245@gmail.com>
2026-04-27 11:48:40 +0900

    [Feat] #304 - StoreService 생성

---
+++ backend/src/main/java/com/example/nexus/store/StoreService.java
@@ -0,0 +1,11 @@
+package com.example.nexus.store;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StoreService {
+    private final StoreRepository storeRepository;
+}

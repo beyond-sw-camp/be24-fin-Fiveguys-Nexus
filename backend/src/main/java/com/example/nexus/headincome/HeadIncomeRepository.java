feb75187b53ceb1161835a419f60a6ef4b2ad064
jdh <wjdehd555@gmail.com>
2026-04-27 15:00:58 +0900

    [Feat] #342 - HeadIncome Repository 구현
    - HeadIncomeRepository JpaRepository 상속으로 생성

---
+++ backend/src/main/java/com/example/nexus/headincome/HeadIncomeRepository.java
@@ -0,0 +1,7 @@
+package com.example.nexus.headincome;
+
+import com.example.nexus.headincome.model.HeadIncome;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HeadIncomeRepository extends JpaRepository<HeadIncome, Long> {
+}

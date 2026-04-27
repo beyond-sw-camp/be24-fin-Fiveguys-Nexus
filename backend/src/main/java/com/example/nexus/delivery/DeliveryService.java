d512a9da06790824370d5236510fde551ccf18b7
shtmdcks06 <shtmdcks06@gmail.com>
2026-04-27 15:44:33 +0900

    [Feat] #346 - Delivery Service 생성

---
+++ backend/src/main/java/com/example/nexus/delivery/DeliveryService.java
@@ -0,0 +1,10 @@
+package com.example.nexus.delivery;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeliveryService {
+    private final DeliveryRepository deliveryRepository;
+}

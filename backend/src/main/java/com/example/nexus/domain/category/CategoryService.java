8a62acaadd4267a36a0e72d5ef84dfa0acf213c1
shtmdcks06 <shtmdcks06@gmail.com>
2026-04-28 17:43:44 +0900

    [Feat] #50 - 제품 카테고리 리스트로 조회 - service 구현

--- backend/src/main/java/com/example/nexus/domain/category/CategoryService.java
+++ backend/src/main/java/com/example/nexus/domain/category/CategoryService.java
@@ -5,6 +5,9 @@ import com.example.nexus.domain.category.model.CategoryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
@@ -13,5 +16,10 @@ public class CategoryService {
     public CategoryDto.RegRes register(CategoryDto.RegReq dto) {
         Category entity = categoryRepository.save(dto.toEntity());
         return CategoryDto.RegRes.from(entity);
+    }
+
+    public List<CategoryDto.ListRes> findAllCategories() {
+        List<Category> categoryList = categoryRepository.findAll();
+        return categoryList.stream().map(CategoryDto.ListRes::from).toList();
     }
 }

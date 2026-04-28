a10771fd11e71e6882627fab5714ce987376ada6
hijaehyuk <messi300060@gmail.com>
2026-04-28 10:19:30 +0900

    [Feat] 폐기(waste_log) 도메인 엔티티 관게 추가

--- backend/src/main/java/com/example/nexus/domain/wastelog/model/WasteLog.java
+++ backend/src/main/java/com/example/nexus/domain/wastelog/model/WasteLog.java
@@ -1,10 +1,15 @@
 package com.example.nexus.domain.wastelog.model;
 
+import com.example.nexus.domain.product.model.Product;
+import com.example.nexus.domain.store.model.Store;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -37,9 +42,11 @@ public class WasteLog {
     @Column(name = "waste_reason", nullable = false)
     private String wasteReason;
 
-    @Column(name = "store_idx", nullable = false)
-    private Long storeIdx;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_idx", nullable = false)
+    private Store store;
 
-    @Column(name = "product_idx", nullable = false)
-    private Long productIdx;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_idx", nullable = false)
+    private Product product;
 }

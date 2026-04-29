7436b78cf001cf52d54ba1a48a44aff398e6a61a
shtmdcks06 <shtmdcks06@gmail.com>
2026-04-29 15:24:17 +0900

    [Feat] #36 - Product Service 신규 제품 등록 기능 구현

--- backend/src/main/java/com/example/nexus/domain/product/ProductService.java
+++ backend/src/main/java/com/example/nexus/domain/product/ProductService.java
@@ -1,10 +1,29 @@
 package com.example.nexus.domain.product;
 
+import com.example.nexus.domain.category.CategoryRepository;
+import com.example.nexus.domain.category.model.Category;
+import com.example.nexus.domain.product.model.Product;
+import com.example.nexus.domain.product.model.ProductDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    @Transactional
+    public ProductDto.RegRes addProduct(ProductDto.RegReq dto) {
+        Category category = categoryRepository.findById(dto.getCategoryIdx()).orElse(null);
+
+        if (category != null) {
+            Product product = dto.toEntity(category);
+            Product saved = productRepository.save(product);
+            return ProductDto.RegRes.from(saved);
+        }
+        return null;
+    }
 }

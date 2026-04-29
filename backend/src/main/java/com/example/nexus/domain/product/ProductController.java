faad502cf68864334b1ab43c7c02bc66a0162aff
shtmdcks06 <shtmdcks06@gmail.com>
2026-04-29 15:23:18 +0900

    [Feat] #36 - ProductController 신규 제품 등록 기능 구현

--- backend/src/main/java/com/example/nexus/domain/product/ProductController.java
+++ backend/src/main/java/com/example/nexus/domain/product/ProductController.java
@@ -1,6 +1,11 @@
 package com.example.nexus.domain.product;
 
+import com.example.nexus.domain.product.model.ProductDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
+
+    // 신규 제품 등록
+    @PostMapping("/reg")
+    public ResponseEntity<ProductDto.RegRes> addNewProduct(@Valid @RequestBody ProductDto.RegReq dto) {
+        ProductDto.RegRes result = productService.addProduct(dto);
+        return ResponseEntity.ok(result);
+    }
 }

6609ca86aaee4519a9c044c0655cdbc820127200
shtmdcks06 <shtmdcks06@gmail.com>
2026-04-29 10:03:30 +0900

    [Feat] #48 - 카테고리 컨트롤러 삭제 기능 구현

--- backend/src/main/java/com/example/nexus/domain/category/CategoryController.java
+++ backend/src/main/java/com/example/nexus/domain/category/CategoryController.java
@@ -25,4 +25,10 @@ public class CategoryController {
         List<CategoryDto.ListRes> dto = categoryService.findAllCategories();
         return ResponseEntity.ok(dto);
     }
+
+    @DeleteMapping("/{idx}")
+    public ResponseEntity deleteCategory(@PathVariable Long idx) {
+        categoryService.deleteCategory(idx);
+        return ResponseEntity.ok("success delete category");
+    }
 }

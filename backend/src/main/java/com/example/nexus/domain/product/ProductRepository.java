6a4ce0f11e1c149c9b86cb32d4974dfec2c3850f
shtmdcks06 <shtmdcks06@gmail.com>
2026-04-30 11:07:41 +0900

    [Feat] #44 - 제품 검색 기능 구현 -제품 레포지토리 productName 을 포함하고 있는 것들을 List로 저장하도록 구현

--- backend/src/main/java/com/example/nexus/domain/product/ProductRepository.java
+++ backend/src/main/java/com/example/nexus/domain/product/ProductRepository.java
@@ -2,6 +2,8 @@ package com.example.nexus.domain.product;
 
 import com.example.nexus.domain.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByProductNameContaining(String productName);
 }

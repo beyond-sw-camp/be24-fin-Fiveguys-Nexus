d9115f4dec7d9a3d9955f208f7548c8d97cbeb04
jdh <wjdehd555@gmail.com>
2026-04-30 12:45:21 +0900

    [Feat] #457 - OrdersRepository에 JpaSpecificationExecutor 추가

--- backend/src/main/java/com/example/nexus/domain/orders/OrdersRepository.java
+++ backend/src/main/java/com/example/nexus/domain/orders/OrdersRepository.java
@@ -4,10 +4,11 @@ import com.example.nexus.common.enums.OrdersStatus;
 import com.example.nexus.common.enums.OrdersType;
 import com.example.nexus.domain.orders.model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface OrdersRepository extends JpaRepository<Orders, Long> {
+public interface OrdersRepository extends JpaRepository<Orders, Long>, JpaSpecificationExecutor<Orders> {
     List<Orders> findAllByOrdersTypeAndOrdersStatus(OrdersType ordersType, OrdersStatus ordersStatus);
     List<Orders> findAllByStore_IdxAndOrdersStatus(Long storeIdx, OrdersStatus orderStatus);
     List<Orders> findAllByOrdersStatus(OrdersStatus ordersStatus);

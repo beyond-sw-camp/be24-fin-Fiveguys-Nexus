0be4e26605f461732e1b5d646e17d17c476f4a29
dwg245 <dwg0245@gmail.com>
2026-05-06 12:28:58 +0900

    [Refactor] #58 - 메뉴 조회 시 삭제 여부 False만 조회하게 수정

--- backend/src/main/java/com/example/nexus/domain/menu/MenuRepository.java
+++ backend/src/main/java/com/example/nexus/domain/menu/MenuRepository.java
@@ -1,14 +1,21 @@
 package com.example.nexus.domain.menu;
 
 import com.example.nexus.domain.menu.model.Menu;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    // 가맹점명 중복 여부
     boolean existsByMenuName(String menuName);
 
+    // 메뉴 이름 중복 확인
     boolean existsByMenuNameAndIdxNot(String menuName, Long idx);
 
+    // 메뉴 삭제 여부 확인
     Optional<Menu> findByIdxAndIsDeletedFalse(Long menuIdx);
+
+    Page<Menu> findAllByIsDeletedFalse(Pageable pageable);
 }

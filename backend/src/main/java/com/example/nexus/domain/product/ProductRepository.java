24eac432e34f670ad521ac536c7bcdc8ab7a6e55
shtmdcks06 <shtmdcks06@gmail.com>
2026-05-06 09:37:48 +0900

    [Feat] #40 - 가맹점 별 제품 리스트 조회 기능 구현 -Repository에서 storeInventory(매장 재고) 기준으로 해당 매장 재고가 있는 제품만 조회 -p:productDto / s:storeInventory

--- backend/src/main/java/com/example/nexus/domain/product/ProductRepository.java
+++ backend/src/main/java/com/example/nexus/domain/product/ProductRepository.java
@@ -2,8 +2,16 @@ package com.example.nexus.domain.product;
 
 import com.example.nexus.domain.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByProductNameContaining(String productName);
+
+    @Query("SELECT p FROM Product p " +
+            "JOIN StoreInventory s ON s.product.idx = p.idx " +
+            "WHERE s.store.idx = :storeIdx")
+    List<Product> findAllByStoreIdx(@Param("storeIdx") Long storeIdx);
 }

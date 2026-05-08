80e1a8c0c6b88e84f5943d32411979bf29ecdf49
shtmdcks06 <shtmdcks06@gmail.com>
2026-05-08 18:21:37 +0900

    [Feat] #693 - DeliveryDto 페이징 처리 응답 Dto 생성

--- backend/src/main/java/com/example/nexus/domain/delivery/model/DeliveryDto.java
+++ backend/src/main/java/com/example/nexus/domain/delivery/model/DeliveryDto.java
@@ -2,8 +2,10 @@ package com.example.nexus.domain.delivery.model;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.springframework.data.domain.Page;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Builder
@@ -31,6 +33,27 @@ public class DeliveryDto {
                 .estimatedArrivalAt(delivery.getEstimatedArrivalAt())
                 .deliveredDate(delivery.getDeliveredDate())
                 .build();
+    }
+
+    // 페이징 응답을 위한 DTO 추가
+    @Getter
+    @Builder
+    public static class DeliveryPageRes {
+        private List<DeliveryDto> deliveryList;
+        private int totalPage;
+        private long totalCount;
+        private int currentPage;
+        private int currentSize;
+
+        public static DeliveryPageRes from(Page<Delivery> page) {
+            return DeliveryPageRes.builder()
+                    .deliveryList(page.getContent().stream().map(DeliveryDto::from).toList())
+                    .totalPage(page.getTotalPages())
+                    .totalCount(page.getTotalElements())
+                    .currentPage(page.getNumber())
+                    .currentSize(page.getSize())
+                    .build();
+        }
     }
 
     // 본사 지연 사유 입력시 Request Body로 받을 정적 내부 클래스 추가

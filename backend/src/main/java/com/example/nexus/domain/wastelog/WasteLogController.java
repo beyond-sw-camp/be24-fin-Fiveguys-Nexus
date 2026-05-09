75655c2e51e01acbf48718688e2e0679cae04c28
KwonLaptop <chris03232000@gmail.com>
2026-05-09 14:43:53 +0900

    [Feat] #706 - 모든 매장 재고 조회하는 기능 추가

--- backend/src/main/java/com/example/nexus/domain/wastelog/WasteLogController.java
+++ backend/src/main/java/com/example/nexus/domain/wastelog/WasteLogController.java
@@ -1,11 +1,48 @@
 package com.example.nexus.domain.wastelog;
 
+import com.example.nexus.domain.pos.model.PosStoreInventoryDto;
+import com.example.nexus.domain.store.StoreInventoryRepository;
+import com.example.nexus.domain.store.StoreService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping("/wastelog")
 @RestController
 @RequiredArgsConstructor
 public class WasteLogController {
+
+    private final StoreService storeService;
+
+    // 유통기한 지난 재고 조회
+    @GetMapping("/over/due-date")
+    public ResponseEntity<List<PosStoreInventoryDto>> findOverDueDateProducts () {
+
+        // store_inventory 와 pos_store_inventory 테이블에서 manufactured_date와
+        // product 테이블에서 danger_days 를 더해서
+        // 오늘 날짜와 비교해서 이전이라면
+        // 폐기 리스트에 담아주기
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 }

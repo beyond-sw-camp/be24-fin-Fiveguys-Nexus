2fa5b8dddc9566e8f0bc4d3a8a78d3ca65b95882
dwg245 <dwg0245@gmail.com>
2026-05-19 16:10:24 +0900

    [Feat] #839 - 게이트웨이 설정

--- backend/gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ backend/gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -3,8 +3,9 @@ package com.example.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class GatewayApplication {
+public class    GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);

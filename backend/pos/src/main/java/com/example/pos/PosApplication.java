0d0aeecec48692ea9dfaf756c66cfcf92b2d7c29
dwg245 <dwg0245@gmail.com>
2026-05-19 09:23:19 +0900

    [Refactor] #831 - POS 모듈 파일 가져오기 및 간단한 세팅

--- backend/pos/src/main/java/com/example/pos/PosApplication.java
+++ backend/pos/src/main/java/com/example/pos/PosApplication.java
@@ -1,4 +1,4 @@
-package org.example.pos;
+package com.example.pos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ backend/pos/src/main/java/com/example/pos/domain/user/model/AuthUserDetails.java
@@ -0,0 +1,57 @@
+package com.example.pos.domain.user.model;
+
+import com.example.pos.common.enums.Role;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Builder
+public class AuthUserDetails implements UserDetails {
+
+    private Long idx;
+    private String username;
+    private String password;
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (role == null) return List.of();
+        return List.of(new SimpleGrantedAuthority(role.toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}

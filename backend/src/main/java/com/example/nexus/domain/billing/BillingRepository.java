0fecd74a229ae9f4ceac38098c7e881e7c712f02
shtmdcks06 <shtmdcks06@gmail.com>
2026-05-21 13:40:25 +0900

    [Feat] #840 - BillingRepository -Billing 정보 storeIdx 기반으로 가져오고 삭제

--- backend/src/main/java/com/example/nexus/domain/billing/BillingRepository.java
+++ backend/src/main/java/com/example/nexus/domain/billing/BillingRepository.java
@@ -3,5 +3,9 @@ package com.example.nexus.domain.billing;
 import com.example.nexus.domain.billing.model.Billing;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BillingRepository extends JpaRepository<Billing, Long> {
+    Optional<Billing> findByStoreIdx(Long storeIdx);
+    void deleteByStoreIdx(Long storeIdx);
 }

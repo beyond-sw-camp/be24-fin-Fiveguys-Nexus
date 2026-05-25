4b0dfa43a0b772d696a28c18448e2b0548b17a7a
kwon_pc <chris0540@naver.com>
2026-05-25 23:12:31 +0900

    [Feat] #875 - 매월 10일 결제 실패건에 대해서 익일에 다시 결제하는 기능 구현

--- backend/billing-batch/src/main/java/org/example/spring/billingbatch/repository/FailedPaymentRepository.java
+++ backend/billing-batch/src/main/java/org/example/spring/billingbatch/repository/FailedPaymentRepository.java
@@ -1,6 +1,8 @@
 package org.example.spring.billingbatch.repository;
 
 import org.example.spring.billingbatch.model.FailedPayment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import java.util.List;
 
 @Repository
 public interface FailedPaymentRepository extends JpaRepository<FailedPayment, Long> {
-    List<FailedPayment> findByPayedMonth(String payedMonth);
+    Page<FailedPayment> findByPayedMonth(String payedMonth, Pageable pageable);
     boolean existsByStoreIdxAndPayedMonth(Long storeIdx, String payedMonth);
 }

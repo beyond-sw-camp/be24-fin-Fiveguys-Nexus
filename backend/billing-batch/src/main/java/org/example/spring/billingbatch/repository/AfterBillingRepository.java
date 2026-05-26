7660838bdf7d8ee3d74f5d4130d252a5cdf57744
shtmdcks06 <shtmdcks06@gmail.com>
2026-05-26 13:28:35 +0900

    AfterBillingRepository -List import

--- backend/billing-batch/src/main/java/org/example/spring/billingbatch/repository/AfterBillingRepository.java
+++ backend/billing-batch/src/main/java/org/example/spring/billingbatch/repository/AfterBillingRepository.java
@@ -3,6 +3,7 @@ package org.example.spring.billingbatch.repository;
 import org.example.spring.billingbatch.model.AfterBilling;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AfterBillingRepository extends JpaRepository<AfterBilling, Long> {
